e6f66bf72b790bf3cc6abd3a0db9f62a266af0ab
Avon46 <142923631+Avon46@users.noreply.github.com>
2026-06-18 10:23:50 +0800

    add router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/premium-calculator',
+    },
+    {
+      path: '/premium-calculator',
+      name: 'premium-calculator',
+      component: () => import('@/views/PremiumCalculatorView.vue'),
+    },
+    {
+      path: '/plan-admin',
+      name: 'plan-admin',
+      component: () => import('@/views/PlanAdminView.vue'),
+    },
+    {
+      path: '/recommend',
+      name: 'recommend',
+      component: () => import('@/views/RecommendView.vue'),
+    },
+  ],
 })
 
 export default router
